c6f2b88eccdade33047e8cbe4a436bc5b292f4e1
Mikal Hitsøy Henriksen <stylpe@gmail.com>
2024-03-09 13:22:01 +0100

    Formatting and refactor if to switch with patterns

--- Controller.cs
+++ Controller.cs
@@ -26,7 +26,7 @@ internal sealed class Controller : IDisposable
         // Receive first message indicating client's native byte order
         var result = await webSocket.ReceiveAsync(mem, ct);
         CheckResult(result);
-        Func<Memory<byte>,short> cast = m => MemoryMarshal.Cast<byte, short>(m.Span)[0];
+        Func<Memory<byte>, short> cast = m => MemoryMarshal.Cast<byte, short>(m.Span)[0];
         short endiannessTest = cast(mslice);
 
         if (endiannessTest != 1)
@@ -40,7 +40,8 @@ internal sealed class Controller : IDisposable
         while (!ct.IsCancellationRequested && webSocket.State is WebSocketState.Open)
         {
             result = await webSocket.ReceiveAsync(mem, ct);
-            if(result.MessageType == WebSocketMessageType.Close) {
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
                 logger.LogInformation("Client disconnected");
                 await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Bye!", ct);
                 return;
@@ -55,8 +56,13 @@ internal sealed class Controller : IDisposable
 
     private static void CheckResult(ValueWebSocketReceiveResult result)
     {
-        if (!result.EndOfMessage) throw new InvalidDataException("Received a partial message... Surely this will never happen in practice?");
-        if (result.Count != 2) throw new InvalidDataException($"Message length was invalid: {result.Count} (should always be 2)");
+        switch (result)
+        {
+            case { EndOfMessage: false }:
+                throw new InvalidDataException("Received a partial message... Surely this will never happen in practice?");
+            case { MessageType: WebSocketMessageType.Binary, Count: not 2 }:
+                throw new InvalidDataException($"Message length was invalid: {result.Count} (should always be 2)");
+        }
     }
 
     public void Dispose()
